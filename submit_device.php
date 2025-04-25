60c232285109bc90b119941530ecb413451ceef7
Rylan Craig <craigrylan94@gmail.com>
2025-04-25 10:14:06 -0400

    Create submit_device.php

---
+++ submit_device.php
@@ -0,0 +1,32 @@
+<?php
+$host = "localhost";
+$username = "root"; 
+$password = "";     
+$dbname = "smart_kitchen"; 
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Get values from form
+$deviceName = $_POST['deviceName'];
+$deviceStatus = $_POST['deviceStatus'];
+$logDetails = $_POST['logDetails'];
+
+// Prepare & bind
+$stmt = $conn->prepare("INSERT INTO device_status (DeviceName, Status, LogDetails) VALUES (?, ?, ?)");
+$stmt->bind_param("sss", $deviceName, $deviceStatus, $logDetails);
+
+// Execute
+if ($stmt->execute()) {
+  echo "Device added successfully!";
+} else {
+  echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$conn->close();
